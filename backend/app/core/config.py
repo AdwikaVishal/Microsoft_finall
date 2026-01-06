2bf5f0053591580f257e9a52434622955a9ef491
AdwikaVishal <168633141+AdwikaVishal@users.noreply.github.com>
2026-01-06 21:18:33 +0530

    Update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
 
     # JWT
     JWT_SECRET: str
-    JWT_ALGORITHM: str = "HSHS256"
+    JWT_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     # Admin bootstrap user
